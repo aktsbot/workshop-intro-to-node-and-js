4e5df5e6140b148f20c8b162911e1ed437234107
akts <box.ashishk@gmail.com>
2019-04-11 23:34:19 +0530

    hello world

---
+++ 00-hello-world/script.js
@@ -0,0 +1,15 @@
+// 1
+console.log("Hello World");
+
+// 2
+// var elToChange = document.getElementById("change-me");
+
+// 3
+// var elToChange = document.getElementById("change-me");
+// var changeBtn = document.getElementById("change-btn");
+
+// changeBtn.addEventListener("click", function() {
+//   elToChange.innerHTML = "Wohoo!";
+// });
+
+// 4
---
+++ README.md
@@ -0,0 +1,12 @@
+# Intro to JavaScript with Node.js
+
+## To run a simple server
+
+```
+$ python -m SimpleHTTPServer
+Serving HTTP on 0.0.0.0 port 8000 ...
+```
+
+The content of the folder from where you ran the above
+command, is now accessible via the browser at
+[http://localhost:8000](http://localhost:8000).
