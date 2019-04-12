0b3053f00e71c1838fc4cb5b6d35825a266014ca
akts <box.ashishk@gmail.com>
2019-04-13 00:58:40 +0530

    npm

---
+++ 04-npm/app.js
@@ -0,0 +1,5 @@
+const lodash = require("lodash");
+
+const myFavMovies = ["Saw I", "Saw II", "Shawshank Redemption"];
+console.log(lodash.indexOf(myFavMovies, "Saw III"));
+console.log(lodash.reverse(myFavMovies));
