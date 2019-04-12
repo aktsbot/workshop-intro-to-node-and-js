a113ebbb48e3eb92e6d487002657446fb458d3c1
akts <box.ashishk@gmail.com>
2019-04-13 00:28:52 +0530

    node basics

---
+++ 03-node-basics/main.js
@@ -0,0 +1,6 @@
+const utils = require("./utils");
+// const { greeting } = require("./utils");
+
+// console.log(greeting);
+console.log(utils.greeting);
+console.log(utils.add(3, 4));
---
+++ 03-node-basics/utils.js
@@ -0,0 +1,10 @@
+const greeting = "Hello you beautiful people";
+
+const add = function(a, b) {
+  return a + b;
+};
+
+module.exports = {
+  greeting: greeting,
+  add: add
+};
